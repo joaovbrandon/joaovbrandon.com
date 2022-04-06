1ab7bdd04a0591d48ff959f15e124e1d815d09da
Brandon <hello@joaovbrandon.com>
2022-04-06 02:59:33 -0300

    fix: styled-components configs (add serverStyleSheet in the document) + eslintrc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,40 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = context.renderPage;
+
+    try {
+      context.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(context);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
